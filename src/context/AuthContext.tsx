85be942270b87afaa5e147edd3b606d1c263cd38
jackmanning95 <jackmanning70@gmail.com>
2025-05-18 16:49:01 +1200

    Update AuthContext with site URL

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -133,7 +133,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         email,
         password,
         options: {
-          emailRedirectTo: `${window.location.origin}/login`
+          emailRedirectTo: `${import.meta.env.VITE_SUPABASE_SITE_URL}/login`
         }
       });
 
